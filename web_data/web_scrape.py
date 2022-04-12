a1ca07886f6396e6fefc1a3b6c3f2a01253bdb76
Andrew Luo <ajluo2@illinois.edu>
2022-04-12 16:16:35 -0500

    Wrote script for scraping uiuc page information. Added covid19 and admission subdomains

---
+++ web_data/web_scrape.py
@@ -0,0 +1,72 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+test_url = "https://admissions.illinois.edu"
+dict_href_links = {}
+set_links_to_visit = set()
+
+def get_data(url):
+    r = requests.get(url)
+    return r.text
+
+def scrape_and_save(url, file_path="covid19/", filename=0):
+    html_data = get_data(url)
+    soup = BeautifulSoup(html_data, "html.parser")
+    paragraph_data = soup.find_all("p")
+
+    f = open(file_path+str(filename)+".txt", "w")
+    f.write(url + "\n")
+    f.write(soup.title.string + "\n")
+    for p in paragraph_data:
+        # print(p.text)
+        f.write(str(p.text) + "\n")
+    # print(paragraph_data)
+
+def get_links(website_link):
+    html_data = get_data(website_link)
+    soup = BeautifulSoup(html_data, "html.parser")
+    list_links = []
+    for link in soup.find_all("a", href=True):
+        # Append to list if new link contains original link
+        if str(link["href"]).startswith((str(website_link))):
+            print("link =", link["href"])
+            list_links.append(link["href"])
+            set_links_to_visit.add(link["href"])
+        # Include all href that do not start with website link but with "/"
+        elif str(link["href"]).startswith("/"):
+            #print("----Test-------")
+            if link["href"] not in dict_href_links:
+                # print(link["href"])
+                dict_href_links[link["href"]] = None
+                link_with_www = website_link +"/"+ link["href"][1:]
+                print("adjusted link =", link_with_www)
+                list_links.append(link_with_www)
+                set_links_to_visit.add(link_with_www)
+        elif "http" not in str(link["href"]) and "html" in str(link["href"]):
+            if link["href"] not in dict_href_links:
+                # print(link["href"])
+                dict_href_links[link["href"]] = None
+                link_with_www = website_link +"/"+ link["href"]
+                print("adjusted link 2 =", link_with_www)
+                list_links.append(link_with_www)
+                set_links_to_visit.add(link_with_www)
+            
+
+                
+    # Convert list of links to dictionary and define keys as the links
+    dict_links = dict.fromkeys(list_links, False)
+    return dict_links
+
+def main():
+    get_links(test_url)
+    ctr = 1
+    for link in set_links_to_visit:
+        scrape_and_save(link, file_path="admissions/", filename=ctr)
+        sleep(1)
+        ctr += 1
+    # scrape_and_save(test_url, filename=1)
+
+
+if __name__ == "__main__":
+    main()
