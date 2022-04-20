3d101c9f877672de1d7d9785654effff4d852baa
Harita Reddy <harita_1997@yahoo.com>
2022-04-20 00:23:58 -0500

    Adding basic bert code

--- text_retriever.py
+++ text_retriever.py
@@ -1,4 +1,6 @@
-import sentence_transformers
+from sentence_transformers import SentenceTransformer
+from sklearn import metrics
+import re
 import os
 
 
@@ -7,15 +9,28 @@ class TextRetriever:
     def __init__(self) -> None:
         pass
 
-    def compute_similarity_score(text_1, text_2):
-        pass
+    def preprocess_text(self, text):
+        non_alpha_chars = re.compile('[^A-Za-z]')
+        processed_text = re.sub('  ', ' ', non_alpha_chars.sub(' ', text))
+        return processed_text
+
+    def compute_similarity_score(self, text_1, text_2):
+        return metrics.pairwise.cosine_similarity(text_1, text_2)
     
     def get_vector_representation(self, text):
-        bert_model = sentence_transfomers.SentenceTransformer('bert-base-nli-mean-tokens')
+        bert_model = SentenceTransformer('bert-base-nli-mean-tokens')
         embedded_text = bert_model.encode(text)
         return embedded_text
 
 
 retriever_object = TextRetriever()
-print(retriever_object.get_vector_representation('covid-19 mask mandate'))
+print(retriever_object.preprocess_text('masks are useful for preventing covid-19'))
+"""
+rep1 = retriever_object.get_vector_representation('covid-19 mask mandate')
+rep2 = retriever_object.get_vector_representation('masks are useful for preventing covid-19')
+rep3 = retriever_object.get_vector_representation('the treasure is located in the depths of the ocean')
+print(retriever_object.compute_similarity_score([rep1], [rep2]))
+print(retriever_object.compute_similarity_score([rep1], [rep3]))
+"""
+
 #retriever_object('covid-19 mask mandate', 'masks are useful for preventing covid-19')
