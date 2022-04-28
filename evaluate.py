4cc1fa5081b516c951bc763a59ae32b5ceb0c76b
akhila-ashokan <akhila.ashokan@vanderbilt.edu>
2022-04-28 13:51:28 -0500

    adding more stuff to evaluate

--- evaluate.py
+++ evaluate.py
@@ -11,36 +11,36 @@ if __name__ == "__main__":
     ctr = 0
     total_questions = 0 
 
-    # retrieve the entire test set and call the retriever on each question
-    with open('testing_data/test_set.txt') as f:
-        for line in f:
-            total_questions += 1
-            nextline = next(f)
-            nextline = nextline.strip()
-            if nextline[-1] == '/':
-                nextline = nextline[0:len(nextline)-1]
-            nextline = nextline.lower()
+    # # retrieve the entire test set and call the retriever on each question
+    # with open('testing_data/test_set.txt') as f:
+    #     for line in f:
+    #         total_questions += 1
+    #         nextline = next(f)
+    #         nextline = nextline.strip()
+    #         if nextline[-1] == '/':
+    #             nextline = nextline[0:len(nextline)-1]
+    #         nextline = nextline.lower()
         
-            # get the highest matching docs 
-            top_urls = retriever_object.get_highest_matching_docs(line.strip(), 5)['URL'].tolist()
-            top_urls_clean = []
-            for i in range(len(top_urls)):
-                url = top_urls[i].strip()
-                if url[-1] == '/':
-                    url = url[0:len(url)-1]
-                url = url.lower()
-                top_urls_clean.append(url)
+    #         # get the highest matching docs 
+    #         top_urls = retriever_object.get_highest_matching_docs(line.strip(), 5)['URL'].tolist()
+    #         top_urls_clean = []
+    #         for i in range(len(top_urls)):
+    #             url = top_urls[i].strip()
+    #             if url[-1] == '/':
+    #                 url = url[0:len(url)-1]
+    #             url = url.lower()
+    #             top_urls_clean.append(url)
                 
-            # check if correct url is in top 5 list 
-            if nextline in top_urls_clean:
-                print("Correct Response Found!")
-                print("Q: " +  line.strip())
-                print("A: " + nextline)
-                ctr+=1
-    print(str(ctr) + " correct retrievals")
+    #         # check if correct url is in top 5 list 
+    #         if nextline in top_urls_clean:
+    #             print("Correct Response Found!")
+    #             print("Q: " +  line.strip())
+    #             print("A: " + nextline)
+    #             ctr+=1
+    # print(str(ctr) + " correct retrievals")
 
-    accuracy = ctr/total_questions
-    print("Accuracy: " + str(accuracy))
+    # accuracy = ctr/total_questions
+    # print("Accuracy: " + str(accuracy))
 
 
     with open('testing_data/reddit_query_answers.txt', 'r') as f:
@@ -61,8 +61,9 @@ if __name__ == "__main__":
                 nextline = nextline[0:len(nextline)-1]
             nextline = nextline.lower()
               
-            #get the highest matching docs
-            top_urls = retriever_object.get_highest_matching_docs(query.strip(), 5)['URL'].tolist()
+            # get the highest matching docs
+            top_docs = retriever_object.get_highest_matching_docs(query.strip(), 5)
+            top_urls = top_docs['URL'].tolist()
             top_urls_clean = []
             for i in range(len(top_urls)):
                 url = top_urls[i].strip()
@@ -73,10 +74,16 @@ if __name__ == "__main__":
 
             # check if correct url is in top 5 list 
             if nextline in top_urls_clean:
+                index = top_urls_clean.index(nextline)
                 print("Correct Response Found!")
                 print("Q: " +  query.strip())
                 print("A: " + nextline)
+                print("Similarity Score:" + str(top_docs['Similarity Scores'].tolist()[index])  + "\n")
                 ctr+=1
+            if nextline == "none":
+                print("No Correct Response!")
+                print("Q: " + query.strip())
+                print("Similarity Score:" + str(top_docs['Similarity Scores'].tolist())  + "\n")
     print(str(ctr) + " correct retrievals")
 
     accuracy = ctr/total_questions
