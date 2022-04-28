8b792134419cde08b0649de6cf62ac59d9c074da
akhila-ashokan <akhila.ashokan@vanderbilt.edu>
2022-04-28 15:35:31 -0500

    adding new urls to be scraped

--- evaluate.py
+++ evaluate.py
@@ -11,36 +11,36 @@ if __name__ == "__main__":
     ctr = 0
     total_questions = 0 
 
-    # # retrieve the entire test set and call the retriever on each question
-    # with open('testing_data/test_set.txt') as f:
-    #     for line in f:
-    #         total_questions += 1
-    #         nextline = next(f)
-    #         nextline = nextline.strip()
-    #         if nextline[-1] == '/':
-    #             nextline = nextline[0:len(nextline)-1]
-    #         nextline = nextline.lower()
+    # retrieve the entire test set and call the retriever on each question
+    with open('testing_data/test_set.txt') as f:
+        for line in f:
+            total_questions += 1
+            nextline = next(f)
+            nextline = nextline.strip()
+            if nextline[-1] == '/':
+                nextline = nextline[0:len(nextline)-1]
+            nextline = nextline.lower()
         
-    #         # get the highest matching docs 
-    #         top_urls = retriever_object.get_highest_matching_docs(line.strip(), 5)['URL'].tolist()
-    #         top_urls_clean = []
-    #         for i in range(len(top_urls)):
-    #             url = top_urls[i].strip()
-    #             if url[-1] == '/':
-    #                 url = url[0:len(url)-1]
-    #             url = url.lower()
-    #             top_urls_clean.append(url)
+            # get the highest matching docs 
+            top_urls = retriever_object.get_highest_matching_docs(line.strip(), 5)['URL'].tolist()
+            top_urls_clean = []
+            for i in range(len(top_urls)):
+                url = top_urls[i].strip()
+                if url[-1] == '/':
+                    url = url[0:len(url)-1]
+                url = url.lower()
+                top_urls_clean.append(url)
                 
-    #         # check if correct url is in top 5 list 
-    #         if nextline in top_urls_clean:
-    #             print("Correct Response Found!")
-    #             print("Q: " +  line.strip())
-    #             print("A: " + nextline)
-    #             ctr+=1
-    # print(str(ctr) + " correct retrievals")
+            # check if correct url is in top 5 list 
+            if nextline in top_urls_clean:
+                print("Correct Response Found!")
+                print("Q: " +  line.strip())
+                print("A: " + nextline)
+                ctr+=1
+    print(str(ctr) + " correct retrievals")
 
-    # accuracy = ctr/total_questions
-    # print("Accuracy: " + str(accuracy))
+    accuracy = ctr/total_questions
+    print("Accuracy: " + str(accuracy))
 
 
     with open('testing_data/reddit_query_answers.txt', 'r') as f:
