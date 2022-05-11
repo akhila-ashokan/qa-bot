254f743bb2d9909a6d310f81ec0b901fd0073b41
akhila-ashokan <akhila.ashokan@vanderbilt.edu>
2022-05-11 00:44:46 -0500

    removed comments and added cli argument code

--- evaluate.py
+++ evaluate.py
@@ -17,84 +17,82 @@ if __name__ == "__main__":
     # create text retriever object
     # retriever_object = TextRetriever()
     retriever_object = TextRetrieverSBERTReranking()
+    print("Testing File Name: " + args.test_file)
 
     ctr = 0
     total_questions = 0 
     incorrect_retrievals = []
 
-    # retrieve the entire test set and call the retriever on each question
-    # with open('testing_data/test_set.txt') as f:
-    #     for line in f:
-    #         total_questions += 1
-    #         nextline = next(f)
-    #         nextline = nextline.strip()
-    #         if nextline[-1] == '/':
-    #             nextline = nextline[0:len(nextline)-1]
-    #         nextline = nextline.lower()
-        
-    #         # get the highest matching docs 
-    #         top_urls, display_text = retriever_object.get_highest_matching_docs(line.strip(), 5)['URL'].tolist()
-    #         top_urls_clean = []
-    #         for i in range(len(top_urls)):
-    #             url = top_urls[i].strip()
-    #             if url[-1] == '/':
-    #                 url = url[0:len(url)-1]
-    #             url = url.lower()
-    #             top_urls_clean.append(url)
-                
-    #         # check if correct url is in top 5 list 
-    #         if nextline in top_urls_clean:
-    #             print("Correct Response Found!")
-    #             print("Q: " +  line.strip())
-    #             print("A: " + nextline)
-    #             ctr+=1
-    # print(str(ctr) + " correct retrievals")
-
-    # accuracy = ctr/total_questions
-    # print("Accuracy: " + str(accuracy))
-
-
-    # with open('testing_data/reddit_query_answers.txt', 'r') as f:
-    #     for line in f:
-    #         nextline = next(f)
-    #         total_questions += 1
-
-    #         # query preprocessing  
-    #         line = open("queries/" + line.strip(), "r", encoding='utf-8')
-    #         query = line.read()
-    #         query = query.replace("\n", " ")
-    #         query = query.replace("&amp;#x200B;", "")
-    #         query = query.replace("&gt;", "")
+    if args.test_file == "testing_data/test_set.txt":
+        with open('testing_data/test_set.txt') as f:
+            for line in f:
+                total_questions += 1
+                nextline = next(f)
+                nextline = nextline.strip()
+                if nextline[-1] == '/':
+                    nextline = nextline[0:len(nextline)-1]
+                nextline = nextline.lower()
             
-    #         # correct url preprocessing
-    #         nextline = nextline.strip()
-    #         if nextline[-1] == '/':
-    #             nextline = nextline[0:len(nextline)-1]
-    #         nextline = nextline.lower()
-              
-    #         # get the highest matching docs
-    #         top_docs = retriever_object.get_highest_matching_docs(query.strip(), 5)
-    #         top_urls = top_docs['URL'].tolist()
-    #         top_urls_clean = []
-    #         for i in range(len(top_urls)):
-    #             url = top_urls[i].strip()
-    #             if url[-1] == '/':
-    #                 url = url[0:len(url)-1]
-    #             url = url.lower()
-    #             top_urls_clean.append(url)
+                # get the highest matching docs 
+                top_urls, display_text = retriever_object.get_highest_matching_docs(line.strip(), 5)['URL'].tolist()
+                top_urls_clean = []
+                for i in range(len(top_urls)):
+                    url = top_urls[i].strip()
+                    if url[-1] == '/':
+                        url = url[0:len(url)-1]
+                    url = url.lower()
+                    top_urls_clean.append(url)
+                    
+                # check if correct url is in top 5 list 
+                if nextline in top_urls_clean:
+                    print("Correct Response Found!")
+                    print("Q: " +  line.strip())
+                    print("A: " + nextline)
+                    ctr+=1
 
-    #         # check if correct url is in top 5 list 
-    #         if nextline in top_urls_clean:
-    #             index = top_urls_clean.index(nextline)
-    #             print("Correct Response Found!")
-    #             print("Question: " +  query.strip())
-    #             print("Expected Answer: " + nextline)
-    #             print("Similarity Score:" + str(top_docs['Similarity Scores'].tolist()[index])  + "\n")
-    #             ctr+=1
-    #         else:
-    #             response = "Question: " + str(line.name) + "\n Expected Answer:" + str(nextline) + "\n Given Answers:" +  str(top_urls_clean) + "\n"
-    #             print(response)
-    #             incorrect_retrievals.append(response)  
+    if args.test_file == "testing_data/reddit_query_answers.txt":
+
+        with open('testing_data/reddit_query_answers.txt', 'r') as f:
+            for line in f:
+                nextline = next(f)
+                total_questions += 1
+
+                # query preprocessing  
+                line = open("queries/" + line.strip(), "r", encoding='utf-8')
+                query = line.read()
+                query = query.replace("\n", " ")
+                query = query.replace("&amp;#x200B;", "")
+                query = query.replace("&gt;", "")
+                
+                # correct url preprocessing
+                nextline = nextline.strip()
+                if nextline[-1] == '/':
+                    nextline = nextline[0:len(nextline)-1]
+                nextline = nextline.lower()
+                
+                # get the highest matching docs
+                top_docs = retriever_object.get_highest_matching_docs(query.strip(), 5)
+                top_urls = top_docs['URL'].tolist()
+                top_urls_clean = []
+                for i in range(len(top_urls)):
+                    url = top_urls[i].strip()
+                    if url[-1] == '/':
+                        url = url[0:len(url)-1]
+                    url = url.lower()
+                    top_urls_clean.append(url)
+
+                # check if correct url is in top 5 list 
+                if nextline in top_urls_clean:
+                    index = top_urls_clean.index(nextline)
+                    print("Correct Response Found!")
+                    print("Question: " +  query.strip())
+                    print("Expected Answer: " + nextline)
+                    print("Similarity Score:" + str(top_docs['Similarity Scores'].tolist()[index])  + "\n")
+                    ctr+=1
+                else:
+                    response = "Question: " + str(line.name) + "\n Expected Answer:" + str(nextline) + "\n Given Answers:" +  str(top_urls_clean) + "\n"
+                    print(response)
+                    incorrect_retrievals.append(response)  
 
     if args.test_file == "testing_data/reddit_query_answers_v2.txt":
 
